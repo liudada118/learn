0d1958f17debe0882866b1a9230b1ca6a92f7f46
liudada118 <2382346483@qq.com>
2020-04-30 22:44:43 +0800

    react

--- react/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from  'react'
-import ReactDom from 'react=dom'
-// import CommentApp from './CommentApp'
-
-ReactDom.render(
-    <div>Comment</div>,
-    document.getElementById('root')
-)
