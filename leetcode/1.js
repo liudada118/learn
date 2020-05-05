e91748bb23c6a4495d1e36ea9ab17424e28c6bb5
liudada118 <62827155+liudada118@users.noreply.github.com>
2020-05-05 23:19:39 +0800

    leetcode字符串去重

--- leetcode/1.js
+++ leetcode/1.js
@@ -1,22 +1,12 @@
-const twoSum = function(nums, target) {
-    // 两重循环， 
-    // target , 一重循环， nums[i],  target-nums[i]
-    // 一次就能找到？  {}
-    let map = {}; //对象字面量
-    nums.forEach(function(e,i) {
-    //   console.log(e, i); 
-      map[e] = i // map[]
-      // var name = 'kfd'
-      // shops[0][name]
-    })
-    // console.log(map);
-    for (i = 0; i < nums.length; i++) {
-      let j = map[target - nums[i]];
-      if (j && j !== i) {
-        return [i, j];
-      }
-      console.log(i)
+var b = 'adafa'
+console.log(b)
+let a = []
+for(let i = 0;i<b.length;i++){
+    let str = a
+    // let c =b.indexOf(b[i])
+    if(b.indexOf(b[i])==-1)
+    {
+        str.push(b[i])
     }
-  }
-  console.log( twoSum ([2,7,11,15],9))
-//   console.log(function([2,7,11,15],9))
+}
+console.log(a)
