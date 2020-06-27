041c74222a6f364064d6610e0bce863bb1816bc3
liudada118 <62827155+liudada118@users.noreply.github.com>
2020-06-27 23:16:02 +0800

    js仿sql

---
+++ sql/index.js
@@ -0,0 +1,8 @@
+const sql = [
+    { id: 1, name: 'john', age: 28, sex: 'male' },
+    { id: 2, name: 'bob', age: 33, sex: 'male' },
+    { id: 3, name: 'tom', age: 22, sex: 'male' },
+    { id: 4, name: 'alice', age: 18, sex: 'female' },
+    { id: 5, name: 'rihana', age: 35, sex: 'female' },
+    { id: 6, name: 'sara', age: 20, sex: 'female' }
+]
